48d1016f2c875d5a9e8a90e45cfeb71d45a4e2fe
Evan Prodromou <evan@e14n.com>
2013-03-30 12:06:27 -0400

    Change the copyright notices

--- models/hostcount.js
+++ models/hostcount.js
@@ -2,7 +2,7 @@
 //
 // snapshot of the count of users on a host a particular time
 //
-// Copyright 2013, StatusNet Inc.
+// Copyright 2013, E14N https://e14n.com/
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
