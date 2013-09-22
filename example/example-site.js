e1692adff6f9273cc06530d5ced7c1142fbaba6f
Evan Prodromou <evan@e14n.com>
2013-09-22 13:00:12 -0700

    Example app

---
+++ example/example-site.js
@@ -0,0 +1,22 @@
+var ClientApp = require("../lib/app"),
+    path = require("path");
+
+// Set up the app
+
+var exampleApp = new ClientApp({
+    name: "Example",
+    version: "0.1.0",
+    hostname: "example.localhost",
+    port: "80",
+    views: path.join(__dirname, "views")
+});
+
+// Add another view
+
+exampleApp.get("/about", function(req, res, next) {
+    res.render("about", {title: "About"});
+});
+
+exampleApp.run(function(err) {
+});
+
