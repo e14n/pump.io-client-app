d0b7ea5c3e95dd738100c2fa94550941c1d9193c
Evan Prodromou <evan@e14n.com>
2013-09-23 21:09:07 -0500

    Slightly better stuff

--- example/example-site.js
+++ example/example-site.js
@@ -7,7 +7,7 @@ var exampleApp = new ClientApp({
     name: "Example",
     version: "0.1.0",
     hostname: "example.localhost",
-    port: "80",
+    port: 8080,
     views: path.join(__dirname, "views")
 });
 
