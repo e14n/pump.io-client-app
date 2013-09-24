693f07004358ab629ad7f937b879fb80e3678839
Evan Prodromou <evan@e14n.com>
2013-09-23 21:27:53 -0500

    Listen on port 80

--- example/example-site.js
+++ example/example-site.js
@@ -7,7 +7,7 @@ var exampleApp = new ClientApp({
     name: "Example",
     version: "0.1.0",
     hostname: "example.localhost",
-    port: 8080,
+    port: 80,
     views: path.join(__dirname, "views")
 });
 
